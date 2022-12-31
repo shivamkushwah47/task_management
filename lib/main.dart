c998bc223c8df081143ec51fe3e8f45a25d5eb04
Umesh Narayan <narwariaumeshgmail.com>
2022-12-31 11:56:29 +0530

    project folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+import 'core/get_pages.dart';
+import 'core/routes.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -11,8 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-       debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.splash,
+      getPages: allpages,
     );
   }
 }
-
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  connectivity_plus
+  geolocator_windows
+  share_plus
+  smart_auth
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
